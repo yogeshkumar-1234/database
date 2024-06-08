dd414a8fc02b0f84ae0fce89d8b3d65ef8412079
Yogesh Kumar <yogeshkumarbinitkumar@gmail.com>
2024-06-08 14:27:46 +0530

    added support for select and insert

--- initial.c
+++ initial.c
@@ -20,6 +20,46 @@ typedef struct {
   ssize_t input_length;
 } InputBuffer;
 
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    close_input_buffer(input_buffer);
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer,
+                                Statement* statement) {
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  if (strcmp(input_buffer->buffer, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+  }
+
+void execute_statement(Statement* statement) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      printf("This is where we would do an insert.\n");
+      break;
+    case (STATEMENT_SELECT):
+      printf("This is where we would do a select.\n");
+      break;
+  }
+}
+
+
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
   input_buffer->buffer = NULL;
@@ -45,10 +85,7 @@ void read_input(InputBuffer* input_buffer) {
   input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer->buffer);
-    free(input_buffer);
-}
+
 
 int main(int argc, char* argv[]) {
   InputBuffer* input_buffer = new_input_buffer();
@@ -56,11 +93,29 @@ int main(int argc, char* argv[]) {
     print_prompt();
     read_input(input_buffer);
 
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    if(input_buffer->buffer[0] == '.'){
+        switch(do_meta_command(input_buffer)){
+            case (META_COMMAND_SUCCESS):
+            continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+             printf("Unrecognized argument %s \n",input_buffer->buffer);
+             continue;
+        }
     }
-  }
-}
+
+    Statement statement;
+       switch (prepare_statement(input_buffer, &statement)) {
+        case (PREPARE_SUCCESS):
+        break;
+        case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecognized keyword at start of '%s'.\n",
+               input_buffer->buffer);
+        continue;
+    }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
+   }
+ }
+   
+  
